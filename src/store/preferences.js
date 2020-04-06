5d1a200172375805b64ba884e537f6c259a2666a
Julio Motol <juliomotol89@gmail.com>
2020-04-06 22:15:58 +0800

    Darkmode state fix

--- src/store/preferences.js
+++ src/store/preferences.js
@@ -1,14 +1,22 @@
 export default {
 	namespaced: true,
 	state: {
-		isDarkMode: false
+		isDarkMode: null,
 	},
 	mutations: {
-		setDarkMode: (state, value) => state.isDarkMode = value
+		setDarkMode: (state, value) => (state.isDarkMode = value),
 	},
 	actions: {
 		initTheme(context) {
 			let root = document.getElementsByTagName('html')[0];
+
+			if (context.state.isDarkMode === null) {
+				if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+					context.commit('setDarkMode', true);
+				} else {
+					context.commit('setDarkMode', false);
+				}
+			}
 
 			if (context.state.isDarkMode) {
 				root.className = 'dark-theme';
@@ -19,13 +27,13 @@ export default {
 		toggleDarkMode(context) {
 			let root = document.getElementsByTagName('html')[0];
 
-			if (context.state.isDarkMode) {
-				root.className = 'light-theme';
-			} else {
-				root.className = 'dark-theme';
-			}
-
 			context.commit('setDarkMode', !context.state.isDarkMode);
-		}
-	}
+
+			if (context.state.isDarkMode) {
+				root.className = 'dark-theme';
+			} else {
+				root.className = 'light-theme';
+			}
+		},
+	},
 };
